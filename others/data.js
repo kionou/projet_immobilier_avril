5ea51734423494abc7b8d8250d598317cc93cf90
kionou <kionoumamadou.00@gmail.com>
2022-07-05 17:37:07 +0000

    update bd

--- others/data.js
+++ others/data.js
@@ -3,12 +3,27 @@ mysql = require('mysql')
 
 let base = mysql.createConnection({
     
-    host:'localhost',
-    user:'root',
-    password:'',
+    host:'192.168.64.2',
+    user:'kionou',
+    password:'12345',
     database:'immobilier'
 
 })
 
+
+
+const pg = require('pg-pool');
+require('dotenv').config();
+
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+
+const pool = new pg.Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+});
 
 module.exports=base;
